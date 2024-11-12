678b8ce4a620ced6664f96e2034b5893678f3de4
Vlm326 <morozvv75@gmail.com>
2024-11-12 21:20:21 +0300

    add main

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from  telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler
 import requests
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Бот работает')
     
 async def prazdnik_now(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    with open('prazdnik.txt', 'r') as file:
+        text = file.read()
+    await update.message.reply_text(text)
+    
+def main():
+    token = '7287412207:AAEs9MrRkDLmcJlcOQOUxM6wWq0zOVpriD4'
+    application = Application.builder().token(token).build()
+    application.add_handler(CommandHandler('prazdnik', prazdnik_now))
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
     
     
     
